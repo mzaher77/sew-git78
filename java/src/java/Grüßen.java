5677e6e34a97be72587e41ae2070d48accbfe922
zaher merna <mzaher@student.tgm.ac.at>
2014-03-14 08:49:57 +0100

    m

--- java/src/java/Grüßen.java
+++ java/src/java/Grüßen.java
@@ -3,6 +3,6 @@ package java;
 public class Grüßen {
 	public static void grüßen(){
         System.out.println("Halllo!");
-        System.out.println("wie gehts!");
+        System.out.println("wie gehts!   ");
     }
 }
